67b940dd06dfc2508ef65dbc37fcf4dcc729f1a8
Kelvin52 <suilamchi0709@yahoo.com.hk>
2021-05-05 00:13:46 +1200

    QR scanner is builded. No timer, timer log, and upload timer data

---
+++ app/src/main/java/com/example/qrtimerk/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.qrtimerk
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+private const val CAMERA_REQUEST_CODE = 101
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+
+    private var seconds = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupPermissions()
+        codeScanner()
+    }
+
+
+    private fun codeScanner(){
+       codeScanner = CodeScanner(this, scanner_view)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread {
+                    tv_textView.text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread{
+                    Log.e("Main", "Camera initialization error:${it.message}")
+                }
+            }
+
+        }
+
+    }
+
+
+
+//    private fun Timer(){
+//       val Hander: handler = new Hander();
+//
+//        var hours = seconds / 3600
+//        var minutes = (seconds % 3600) / 60
+//        var secs = second % 60
+//
+//        var time: String = String.format(Locale.getDefault(), "%d:%02d:%02d", hours,
+//        minutes, secs)
+//
+//        if(running){
+//            seconds++
+//        }
+//        handler.postDelayed(this, 1000)
+//    }
+
+    override fun onResume(){
+        super.onResume()
+        codeScanner.startPreview()
+
+    }
+
+    override fun onPause(){
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions(){
+        val permission = ContextCompat.checkSelfPermission(this,
+        android.Manifest.permission.CAMERA)
+
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest(){
+        ActivityCompat.requestPermissions(this,
+        arrayOf(android.Manifest.permission.CAMERA),
+        CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode){
+            CAMERA_REQUEST_CODE -> run {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "You need camera permission to be able to use this app!", Toast.LENGTH_SHORT).show()
+                } else {
+                    //it was successful
+                }
+            }
+        }
+    }
+
+}
